2b1243244f3eb43221b4cd5eeaa3315b297f82d8
Oleg <hellreebok@gmail.com>
2022-10-09 20:55:32 +0300

    update variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT, () => {
     console.log(`Server running. Use our API on port: ${PORT}`);
 });
